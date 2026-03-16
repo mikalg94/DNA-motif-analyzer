c9f25be6a15bc41c79fd8bb1a5998b665bd601f5
michal <mikalg94@gmail.com>
2026-03-16 22:08:35 +0100

    podstawa main

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from src.io_utils import load_sequence_from_txt
-
-
 def main():
-    sequence = load_sequence_from_txt("data/example_sequence.txt")
-    print("Sequence length:", len(sequence))
+    print("DNA Motif Analyzer started")
 
 
 if __name__ == "__main__":
