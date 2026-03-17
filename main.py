d3fb9ba5e2115200800032da44bc655062d5c439
michal <mikalg94@gmail.com>
2026-03-17 21:07:53 +0100

    Dodanie sekwencji z pliku txt

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from src.io_utils import load_sequence_from_txt
 def main():
     sequence = load_sequence_from_txt("data/example_sequence.txt")
     print("Sequence length:", len(sequence))
+    print("Sequence:", sequence)
 
 
 if __name__ == "__main__":
--- src/io_utils.py
+++ src/io_utils.py
@@ -2,7 +2,8 @@ def load_sequence_from_txt(path):
     with open(path, "r") as file:
         sequence = file.read()
 
-    sequence = sequence.replace("\n", "").replace(" ", "")
+    sequence = sequence.replace("\n", "")
+    sequence = sequence.replace(" ", "")
     sequence = sequence.upper()
 
     return sequence
