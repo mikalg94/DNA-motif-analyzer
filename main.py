ea9eb0e9bc02ae0fb84164761f68fedabf76dbbc
michal <mikalg94@gmail.com>
2026-03-19 22:35:00 +0100

    Połączenie wykresu z analizą sekwencji

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.io_utils import load_sequence_from_txt
 from src.motif_analysis import build_statistics_dataframe
+from src.export_utils import plot_motif_distribution
 
 
 def main():
@@ -7,8 +8,9 @@ def main():
     motif = "ATG"
 
     df = build_statistics_dataframe(sequence, motif, segment_length=10)
-
     print(df)
+
+    plot_motif_distribution(df, motif)
 
 
 if __name__ == "__main__":
